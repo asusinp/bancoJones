f31c0ac8e938d2ede8ec13d5175ca8ced72209fa
iaw21752927 <alexsusin1998@gmail.com>
2017-10-09 18:38:53 +0200

    added cosas raras

--- src/DAO/ClienteDAO.java
+++ src/DAO/ClienteDAO.java
@@ -1,10 +1,62 @@
 package DAO;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import bbdd.ConnectionManager;
+import beans.Cliente;
+
 public class ClienteDAO {
-	
-	public static Cliente loginValid() {
+
+	static Connection con = null;
+
+	public static Cliente loginValid(String user, String pass) {
+		Cliente c = new Cliente();
+
+		con = ConnectionManager.getConnection();
+		ResultSet rs = null;
+		PreparedStatement stmt = null;
 		
-		return true;
+		try {
+			stmt = con.prepareStatement("SELECT * FROM clientes WHERE contraseña=" + pass + "\"" + "and dni=\"" + user + "\"");
+			rs = (ResultSet) stmt.executeQuery();
+			if (rs.next()) {
+				c.setNombre(rs.getString("nombre"));
+				c.setDni(rs.getString("dni"));
+				c.setValid(true);
+			} else {
+				c.setValid(false);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+			if (stmt != null) {
+				try {
+					stmt.close();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+			if (con != null) {
+				try {
+					con.close();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+		ConnectionManager.getConnection();
+		return c;
 	}
-	
+
 }
--- src/beans/Cliente.java
+++ src/beans/Cliente.java
@@ -1,5 +1,31 @@
 package beans;
 
-public class Cliente {
 
+public class Cliente {
+	
+	private String nombre;
+	private String dni;
+	private boolean isValid;
+	
+	public boolean isValid() {
+		return isValid;
+	}
+	public void setValid(boolean isValid) {
+		this.isValid = isValid;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getDni() {
+		return dni;
+	}
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+	
+	
+	
 }

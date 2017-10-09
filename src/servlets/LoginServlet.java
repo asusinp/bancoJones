c7a456f84837651bd9f09961c0bce77a024507a5
iaw21752927 <alexsusin1998@gmail.com>
2017-10-09 18:45:38 +0200

    added cosas raras, pero entendibles

---
+++ src/servlets/LoginServlet.java
@@ -0,0 +1,50 @@
+package servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import DAO.ClienteDAO;
+import beans.Cliente;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String dni = request.getParameter("dni");
+		String pass = request.getParameter("pass");		
+		Cliente c = ClienteDAO().loginValid(dni, pass);
+		if (c.isValid()) {
+			request.getRequestDispatcherType("loginok.jsp").include(request,response);
+		} else {
+			request.getRequestDispatcherType("loginko.jsp").include(request,response);
+		}
+	}
+
+}
